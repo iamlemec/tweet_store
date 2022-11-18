325c1ebfd1ffcab11885c2bbaeed504046aed5ed
Douglas Hanley <thesecretaryofwar@gmail.com>
2022-11-17 23:13:21 -0500

    sync up with library changes

--- tweet_store.py
+++ tweet_store.py
@@ -7,15 +7,12 @@ import twitter
 import sqlite3
 import pandas as pd
 
-RATE_LIMIT = 15
-RATE_WINDOW = 15
-MAX_COUNT = 200
+RATE_LIMIT = 100
+TIME_DELAY = 2
+MAX_COUNT = 100
 
-default = {
-    'include_rts': False,
-    'exclude_replies': True,
-    'trim_user': True
-}
+def to_timestamp(ds):
+    return int(pd.to_datetime(ds).timestamp())
 
 class TweetStore:
     def __init__(self, handle, db, table='tweet', auth=None):
@@ -31,39 +28,45 @@ class TweetStore:
             auth = f'creds/{handle}.json'
         with open(auth) as f:
             creds = json.load(f)
-        self.api = twitter.Api(**creds)
+
+        oauth = twitter.OAuth(
+            creds['access_token_key'], creds['access_token_secret'],
+            creds['consumer_key'], creds['consumer_secret']
+        )
+        self.api = twitter.Twitter(auth=oauth)
 
     def __del__(self):
         self.con.close()
 
     def init_db(self):
         cur = self.con.cursor()
-        cur.execute(f'create table if not exists {self.table} (id int, created int, handle text, body text)')
-        cur.execute(f'create unique index if not exists tid on {self.table} (id)')
+        cur.execute(f'CREATE TABLE IF NOT EXISTS {self.table} (id INT, created INT, handle TEXT, body TEXT)')
+        cur.execute(f'CREATE UNIQUE INDEX IF NOT EXISTS tid ON {self.table} (id)')
         self.con.commit()
 
     def sync_batch(self, when='newest', **kwargs):
         cur = self.con.cursor()
 
-        min_id, max_id = cur.execute(f'select min(id),max(id) from {self.table}').fetchone()
+        min_id, max_id = cur.execute(f'SELECT min(id),max(id) FROM {self.table}').fetchone()
         if when == 'newest':
-            iargs = {'since_id': max_id}
+            iargs = {'since_id': max_id} if max_id is not None else {}
         elif when == 'oldest':
-            iargs = {'max_id': min_id - 1 if min_id is not None else None}
+            iargs = {'max_id': min_id - 1} if min_id is not None else {}
         else:
             iargs = {}
 
-        args = {**default, **iargs, **kwargs}
-        stats = self.api.GetUserTimeline(screen_name=self.handle, count=MAX_COUNT, **args)
+        stats = self.api.statuses.user_timeline(screen_name=self.handle, **iargs, **kwargs)
         nrets = len(stats)
 
         print(f'Fetched {nrets} {when} tweets')
         if nrets == 0:
             return 0
 
+        print(stats[0]['id'], ':', stats[0]['text'])
+
         cur.executemany(
-            f'insert or replace into {self.table} values (?,?,?,?)',
-            [(st.id, st.created_at_in_seconds, self.handle, st.text) for st in stats]
+            f'INSERT OR REPLACE INTO {self.table} VALUES (?,?,?,?)',
+            [(st['id'], to_timestamp(st['created_at']), self.handle, st['text']) for st in stats]
         )
         self.con.commit()
 
@@ -77,10 +80,12 @@ class TweetStore:
                 nrets = self.sync_batch(when='oldest', **kwargs)
                 if nrets == 0:
                     done_old = True
+            time.sleep(TIME_DELAY)
             if not done_new:
                 nrets = self.sync_batch(when='newest', **kwargs)
                 if nrets == 0:
                     done_new = True
+            time.sleep(TIME_DELAY)
             if done_old and done_new:
                 return True
         return False
@@ -89,7 +94,6 @@ class TweetStore:
         while True:
             if self.sync_window(**kwargs):
                 break
-            time.sleep(60*RATE_WINDOW)
 
 class TweetView:
     def __init__(self, db, table='tweet'):
@@ -104,13 +108,13 @@ class TweetView:
         self.con.close()
 
     def fetch(self, limit=None):
-        ltxt = f'{limit}' if limit is not None else ''
+        ltxt = f'LIMIT {limit}' if limit is not None else ''
         cur = self.con.cursor()
-        cur.execute(f'select * from {self.table} {ltxt}')
+        cur.execute(f'SELECT * from {self.table} {ltxt}')
         return cur
 
     def fetch_many(self, limit=10):
-        return cur.fetch(limit=limit).fetchall()
+        return self.fetch(limit=limit).fetchall()
 
     def fetch_all(self):
         return self.fetch().fetchall()
